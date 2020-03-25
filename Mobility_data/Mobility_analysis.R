c1f32474fe66dc78e659c777bae756f72d38ed4e
ntaconet <34338667+ntaconet@users.noreply.github.com>
2020-03-25 16:08:37 +0100

    First commit of the code

---
+++ Mobility_data/Mobility_analysis.R
@@ -0,0 +1,18 @@
+# This is the script to analyse the data.
+# Put it in a folder "Mobility_Germany", with 
+# 1/ the folder "CSV" that contains Mobility data
+# 2/ a folder "Other_input" that will receive the other input we will need (emission factors, ...)
+
+###########################
+# STEP 1 : Calculate emissions for all travels ----
+###########################
+
+# Emissions - Wege -----
+Wege<-read.csv("CSV/MiD2017_Wege.csv",sep=";")
+
+#
+
+
+# 
+
+# First create emission variable for all
