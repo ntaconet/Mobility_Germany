e7d740d97a3083d6d7e35d48c66428e28af448e6
ntaconet <34338667+ntaconet@users.noreply.github.com>
2020-06-15 13:35:36 +0200

    Quantile_Regression and exporting Word file

--- Mobility_data/quantile_regressions.R
+++ Mobility_data/quantile_regressions.R
@@ -6,7 +6,7 @@ Person_dataset<-read.csv("Output/Person_dataset.csv")
 #which months have a higher?
 
 Person_dataset<-Person_dataset%>%
-  mutate(P_GEW_num=as.numeric(gsub(",",".",as.character(P_GEW)))) # weights should be numeric
+  mutate(P_GEW_num=as.numeric(gsub(",",".",as.character(P_GEW))))%>% # weights should be numeric
   filter(alter_gr2 %in% c(3:9)) # remove ppl below 20
          #relevel(ST_MONAT,ref=10)) #  
 
@@ -26,43 +26,74 @@ table_variables<-read_excel("Other_input/Table_variables.xlsx")
 ################
 
 # first let's perform quantile regression for the main variables
-
 Independant_variable<-"emissions_RL"
+Weights<-"P_GEW_num"
 
 # Choose the dependent variables
-Variables_tokeep<-subset(table_variables,type %in% c("main","control","attitude","accessibility","other"))
+#Variables_tokeep<-subset(table_variables,type %in% c("main","control","attitude","accessibility","other"))
+Variables_tokeep<-subset(table_variables,type %in% c("main","control","accessibility","other"))
 
-Regression_dataset<-Person_dataset
+Dependant_variables<-Variables_tokeep$label
+
+Regression_dataset<-Person_dataset%>%
+  select(Independant_variable,Variables_tokeep$varname,Weights)%>%
+  na.omit()
+
+Regression_dataset<-Regression_dataset%>%
+  rename_at(vars(as.character(Variables_tokeep$varname)),~as.character(Variables_tokeep$label))
+
 
 # Remove answers corresponding to "No answer"
-for (i in nrow(Variables_tokeep)){
-  # except for MONTHS, which is a factor
-  #if ((class(Regression_dataset[Variables_tokeep$varname[i]])=="factor")==F){
+for (i in 1:nrow(Variables_tokeep)){
+  print(Variables_tokeep$varname[i])
+  
   Regression_dataset<-Regression_dataset%>%
-    filter(get(Variables_tokeep$varname[i])<Variables_tokeep$remove_above[i])
-
-  #}
+    filter(get(Variables_tokeep$label[i])<Variables_tokeep$remove_above[i])
 }
 
 Regression_dataset<-Regression_dataset%>%
-  mutate(ST_MONAT=factor(ST_MONAT),
-         alter_gr2=factor(alter_gr2))
+  mutate(Month=factor(Month),
+         Age=factor(Age))
 
-Dependant_variables<-Variables_tokeep$varname
 
 
 Regression_Quantile<-rq(paste(Independant_variable,"~",paste(Dependant_variables,collapse=" + ")),
                         data=Regression_dataset,
-                        tau=c(0.25,0.5,0.75,0.9))
+                        tau=c(0.25,0.5,0.75,0.9),
+                        weights=P_GEW_num)
 
 # compare to OLS
 Regression_OLS<-lm(paste(Independant_variable,"~",paste(Dependant_variables,collapse=" + ")),
-                   data=Regression_dataset)
+                   data=Regression_dataset,
+                   weights=P_GEW_num)
+
+Regression_OLS_2<-lm(paste(Independant_variable,"~",paste(Dependant_variables,collapse=" + ")),
+                   data=Regression_dataset,
+                   weights=P_GEW_num)
 
 
-Regression_Quantile
-summary(Regression_Quantile,se="ker")
+Regression_Quantile_tau90<-rq(paste(Independant_variable,"~",paste(Dependant_variables,collapse=" + ")),
+                              data=Regression_dataset,
+                              tau=c(0.9),
+                              weights=P_GEW_num)
 
-summary(Regression_OLS)
+Regression_Quantile_tau75<-rq(paste(Independant_variable,"~",paste(Dependant_variables,collapse=" + ")),
+                              data=Regression_dataset,
+                              tau=c(0.75),
+                              weights=P_GEW_num)
 
+
+Regression_Quantile_tau50<-rq(paste(Independant_variable,"~",paste(Dependant_variables,collapse=" + ")),
+                              data=Regression_dataset,
+                              tau=c(0.5),
+                              weights=P_GEW_num)
+
+
+#Regression_Quantile
+#summary(Regression_Quantile,se="ker")
+#summary(Regression_OLS)
+
+export_summs( Regression_OLS,Regression_Quantile_tau90,Regression_Quantile_tau75,Regression_Quantile_tau50, 
+              model.names = c("OLS","Quantile (50%)","Quantile (75%)","Quantile (90%)"),
+              to.file = "docx", file.name = "Comparing_OLS_to_Quantile.docx")
 
